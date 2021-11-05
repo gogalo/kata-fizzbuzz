fb0b22a8806a6fff1d100cddb0eae672ec5d22ea
Gorka <gorka@veiss.com>
2021-11-05 08:01:45 +0100

    REFACTOR: test

--- test/FizzBuzzShould.php
+++ test/FizzBuzzShould.php
@@ -8,24 +8,37 @@ use function PHPUnit\Framework\equalTo;
 
 class FizzBuzzShould extends TestCase
 {
+    protected $data;
+
+    public function setUp(): void
+    {
+        $fizzBuzz = new FizzBuzz();
+        $this->data = $fizzBuzz->generate();
+    }
+
     /**
      * @test
      */
     public function return_an_array_with_100_elements() {
-        $fizzBuzz = new FizzBuzz();
-        $data = $fizzBuzz->generate();
-        $this->assertEquals(100, count($data));
+        $this->assertThat(count($this->data), $this->equalTo(100));
     }
 
     /**
      * @test
      */
     public function elements_goes_from_1_to_100() {
-        $fizzBuzz = new FizzBuzz();
-        $data = $fizzBuzz->generate();
-        $this->assertThat($data[0], $this->equalTo("1"));
-        $this->assertThat($data[8], $this->equalTo("7"));
-        $this->assertThat($data[97], $this->equalTo("98"));
+        $this->assertThat($this->getFizzBuzzValue(1), $this->equalTo("1"));
+        $this->assertThat($this->getFizzBuzzValue(7), $this->equalTo("7"));
+        $this->assertThat($this->getFizzBuzzValue(98), $this->equalTo("98"));
+    }
+
+    /**
+     * @param int $i
+     * @return mixed
+     */
+    private function getFizzBuzzValue(int $i)
+    {
+        return $this->data[$i -1];
     }
 
 }
